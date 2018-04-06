aa49b9a8d4073d52bf9bfa7f12ffd7c44ea078ad
Gabriel Lee <Gabriel@eduroam-169-233-241-151.ucsc.edu>
2018-04-05 18:46:49 -0700

    Version with operational nav bar. Need to add actual content

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 $(document).ready(function(){
 
-	//Nav bar collapse
+	//Nav bar retract
 	$(".navbutton").click(function() {
-		$(this).parent().animate({paddingLeft: "3%", width: "10%"}, 200, "linear");
-		$(this).animate({marginLeft: "0%"}, 200);
+		$(this).parent().animate({paddingLeft: "30px", width: "120px"}, 200, "linear");
+		$(this).animate({marginLeft: "0%"}, 200, "linear");
 		$(this).siblings().hide();
 		var s = $(this).text();
 		$("#" + s + "nav").show();
 		$("#" + s + "nav").children().show();
+	});
+	//Nav bar extend
+	$(".backbutton").click(function() {
+		$("#defaultnav").animate({paddingLeft: "10%", width: "100%"}, 200, "linear");
+		$(".navbutton").animate({marginLeft: "9%"}, 200, "linear");
+		$(".navbutton").show();
+		$(this).hide();
+		$(this).parent().hide();
+		$(this).siblings().hide();
 	});
 
 });
